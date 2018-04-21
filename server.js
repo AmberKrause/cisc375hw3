60fbbf1851a2604f6c71c7e78a57db7f79b4c39d
AmberKrause <amberkrause75@gmail.com>
2018-04-21 12:17:42 -0500

    Results Page Working

--- server.js
+++ server.js
@@ -17,29 +17,6 @@ var port = 3000;
 var sqlHolder = [];
 
 
-
-
-//open the database
-var db = new sqlite3.Database('./imdb.sqlite3', (err) => {
-	if(err) {
-		return console.error(err.message);
-	}
-});
-
-//query test
-/*
-var sql = 'SELECT DISTINCT primary_title FROM Titles';
-
-db.all(sql, [], (err, rows) => {
-	if(err) {
-		console.log('query error');
-	}
-	rows.forEach((row) => {
-		console.log(row.primary_title);
-	});
-});
-*/
-
 //middleware, required to use the modules
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -78,18 +55,8 @@ app.get('/', (req, res) => {
 app.post('/search', (req, res) => {
     req.checkBody('search_text', 'Please provide something to search').notEmpty();
 
-    var sqlSearch = [ //Just a testing variable to print onto results page
-        {
-        search_text: req.body.search_text,
-        category: req.body.category
-        }]
-
-    //use the req.body.x in conjunction with SQL statements below
-    //to possibly make user search possible
-    var sql = 'SELECT * FROM Titles NATURAL JOIN Ratings';
-
-    //console.log(req.headers);
-    //console.log(req.body);
+    console.log(req.headers);
+    console.log(req.body);
 
     //validate form input
     //req.checkBody('search-text', 'Enter text to search').trim().isLength({min: 1});
@@ -107,16 +74,36 @@ app.post('/search', (req, res) => {
     else
     { //Correct input sends you to the results page
 
-        console.log(sqlSearch);
-        getData(sql).then(function(sqlHolder) {//promise
-            //Promise doesn't work correctly, need to refresh
-            //to show the data, else the query works
-            console.log('then function');
-            res.render('results', { //prints out sql to results page
-            sqlHolder: sqlHolder
-            });//res.render end
-            return sqlHolder;
-        });//getData promise end
+			var search_text = req.body.search_text.replace(/\*/g, "%");//use wildcard character
+
+			if(req.body.category === 'title')
+			{
+				console.log('title entered');
+				var sql = 'SELECT tconst, primary_title, title_type, start_year, end_year FROM Titles WHERE primary_title LIKE "' + search_text + '";';
+				console.log(sql);
+			}
+			else if(req.body.category === 'person')
+			{
+				console.log('person entered');
+				var sql = 'SELECT nconst, primary_name, primary_profession, birth_year, death_year FROM Names WHERE primary_name LIKE "' + search_text + '";';
+				console.log(sql);
+			}
+
+      getData(sql, req.body.category).then(function(sqlHolder) {//promise
+          console.log('then function');
+          res.render('results', { //prints out sql to results page
+          sqlHolder: sqlHolder
+          });//res.render end
+          return sqlHolder;
+      	}, function() {
+					//rejection callback
+					//gets called if the query returned no results
+					console.log('query promise rejected')
+					var errors = [{msg:'No results. Please try another search'}];
+					res.render('index', {
+	            errors: errors
+	        });//res.render end
+			});//getData promise end
     }//else
 
 });//app.post end
@@ -130,31 +117,53 @@ function getData(sql, method) { //gets the SQL data
     var rowlength; //to hold rowlength
     var currentrowlength = 0; //to hold current row
     var myPromise = new Promise(function(resolve, reject) {
-        var dbs = new sqlite3.Database('./imdb.sqlite3');
+        var dbs = new sqlite3.Database('./imdb.sqlite3', (err) => {
+					if(err) {
+						return console.error(err.message);
+					}
+				});
         sqlHolder = []; //resets the sqlHolder array to be empty so data doesn't repeat
         dbs.all(sqlItem, [], (err, rows) => { rowlength = rows.length; console.log(rowlength);
         if(err) {// logs error
             console.log(err);
         }//if(err)
-        rows.forEach((row) => { currentrowlength += 1; console.log(currentrowlength);
-            sqlHolder.push({
-            //set Var name: actual variable
-            //for results: data for results
-            primary_title: row.primary_title,
-            title_type: row.title_type,
-            start_year: row.start_year,
-            end_year: row.end_year,
-            runtime_minutes: row.runtime_minutes,
-            genres: row.genres,
-            average_rating: row.average_rating,
-            num_votes: row.num_votes
-        });//sql push
+
+				//reject promise if no rows to prevent infinite loop
+				if(rowlength < 1) {reject('Unsuitable Data for usage because of 0 length!');}
+
+				//fill sqlHolder with data from each row
+				rows.forEach((row) => { currentrowlength += 1;
+						if(method === 'title') {
+
+						    sqlHolder.push({
+				            //set Var name: actual variable
+				            //for results: data for results
+										type: method,
+										tconst: row.tconst,
+				            primary_title: row.primary_title,
+				            title_type: row.title_type,
+				            start_year: row.start_year,
+				            end_year: row.end_year
+								});//sql push
+						} else if(method === 'person') {
+								sqlHolder.push({
+										//set Var name: actual variable
+										//for results: data for results
+										type: method,
+										nconst: row.nconst,
+										primary_name: row.primary_name,
+										primary_profession: row.primary_profession,
+										birth_year: row.birth_year,
+										death_year: row.death_year
+								});//sql push
+						}
+
             if(currentrowlength == rowlength) { resolve(sqlHolder); console.log('Done');}
-            else if(rowlength < 0) {reject('Unsuitable Data for usage because of 0 length!');}
+            else if(rowlength < 1) {reject('Unsuitable Data for usage because of 0 length!');}
             //Resolving and Rejecting if statements, to determine wait for data
         });//forEach
     });//dbs end
     dbs.close()
-    });//my Promise End
+		});//my Promise End
     return myPromise;
 }//getData end
