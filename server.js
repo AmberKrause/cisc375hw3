0a1a89c7c71a3cd68a3a7547ffe7000e0ab89e2e
nunuchi <36165551+nunuchi@users.noreply.github.com>
2018-04-25 23:02:13 -0500

    Update: People and Movies are named and linked

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var url = require('url');
-
+var https = require('https');
 //Sets up Modules for use
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -19,7 +19,7 @@ var sqlHolder = [];
 var sqlTitleArray = [];
 var sqlTitlePrincipalsArray = [];
 var sqlPeopleArray = [];
-
+var posterImage;
 
 //middleware, required to use the modules
 app.use(bodyParser.json());
@@ -196,38 +196,41 @@ app.post('/updateTitle', (req, res) => {
 
 
 //New Get 4-22-18
+//UPDATED CODE BY NOU
 app.get('/title', function(req, res){
-	var titlePromise = getExtendData(req.query.id);
+  var titlePromise = getExtendData(req.query.id);
   var peoplePromise = getTitlePrincipalData(req.query.id);
-
-  Promise.all([titlePromise, peoplePromise]).then(function(values){//promise
-		res.render('title', {
-			sqlTitleArray: values[0],
-      sqlTitlePrincipalsArray: values[1]
+  var posterImagePromise = posterDataFunction (req.query.id);
+  console.log(posterImagePromise+' 123456');
+  Promise.all([titlePromise, peoplePromise, posterImagePromise]).then(function(values){//promise
+    res.render('title', {
+        sqlTitleArray: values[0],
+            sqlTitlePrincipalsArray: values[1],
+            moviePoster: values[2]
     });//res.render end
     //console.log(values);
     //console.log(values[0]);
-		//console.log('Title Array'+' '+values[0]+' Title Principals Array: '+values[1]);
-	}).catch(function(err) {
-      console.log('Error when rendering title page:' + err.message);
+    console.log('Title Array'+' '+values[0]+' Title Principals Array: '+values[1]);
   });//getExtendedData end
 })//app.get end
 
-//New Get 4-22-18
+
 app.get('/person', function(req, res){
-	getPersonData(req.query.id).then(function(sqlPeopleArray){//promise
-		res.render('person', {
-			sqlPeopleArray: sqlPeopleArray
-		});//res.render end
-		console.log('People Array'+sqlPeopleArray)
-	}).catch(function(err) {
-      console.log('Error when rendering person page:' + err.message);
+    var personPromise = getPersonData(req.query.id);
+    var personPosterPromise = posterPersonDataFunction(req.query.id)
+  Promise.all([personPromise, personPosterPromise]).then(function(values){//promise
+    res.render('person', {
+      sqlPeopleArray: values[0],
+            personPoster: values[1]
+    });//res.render end
+    console.log('People Array'+sqlPeopleArray)
   });//getExtendedData end
 })//app.get end
 
 app.listen(port, () => {
-	console.log('Now listening on port ' + port);
+  console.log('Now listening on port ' + port);
 });//app.listen end
+
 
 function getData(sql, method) { //gets the SQL data
     var sqlItem = sql;
@@ -285,41 +288,54 @@ function getData(sql, method) { //gets the SQL data
     return myPromise;
 }//getData end
 
-//New function 4-22-18
+//Updated 4/25/18
 function getExtendData(inputID) {
     //var rowlength; //to hold rowlength
     //var currentrowlength = 0; //to hold current row
 
-    var sqlItem = 'SELECT Titles.tconst AS tconst, primary_title, title_type, start_year, end_year, runtime_minutes, genres, average_rating, num_votes, directors, writers FROM (SELECT * FROM Titles WHERE tconst="'+inputID+'") AS Titles LEFT OUTER JOIN Ratings ON Titles.tconst=Ratings.tconst LEFT OUTER JOIN Crew ON Titles.tconst=Crew.tconst';
+    var sqlItem = 'SELECT tconst, primary_title, title_type, start_year, end_year, runtime_minutes, genres, average_rating, num_votes, directors, writers FROM (SELECT * FROM Titles WHERE tconst="'+inputID+'") NATURAL JOIN Ratings NATURAL JOIN Crew';
     sqlTitleArray = []; //resets the sqlHolder array to be empty so data doesn't repeat
 
-	   var myPromise = new Promise(function(resolve, reject) {
+     var myPromise = new Promise(function(resolve, reject) {
        var dbs = new sqlite3.Database('./imdb.sqlite3', (err) => {
-	        if(err) {
+          if(err) {
             return console.error(err.message);
              }//if(err) end
-	      });//dbs end
-	      console.log(sqlItem);
+        });//dbs end
+        console.log(sqlItem);
         dbs.get(sqlItem, [], (err, row) => {
-        	console.log(row);
-        	if(err) {// logs error
+          console.log(row);
+          if(err) {// logs error
             console.log(err);
-        	}//if(err)
+          }//if(err)
 
-    			//reject promise if no rows to prevent infinite loop
-    			if(row === undefined) {reject('No query results');}
-    			//fill sqlHolder with data
+          //reject promise if no rows to prevent infinite loop
+          if(row === null) {reject('Unsuitable Data for usage because of 0 length!');}
+          //fill sqlHolder with data
 
-            sqlTitleArray = row;
-            console.log('sqlTitleArray:' + sqlTitleArray);
+            sqlTitleArray = {
+                tconst: row.tconst,
+                primary_title: row.primary_title,
+                title_type: row.title_type,
+                start_year: row.start_year,
+                end_year: row.end_year,
+                runtime_minutes: row.runtime_minutes,
+                genres: row.genres,
+                average_rating: row.average_rating,
+                num_votes: row.num_votes,
+                directors: row.directors,
+                writers: row.writers,
+                directorsNamed: getCastNames(row.directors),
+                writersNamed: getCastNames(row.writers)
+            }
             resolve(sqlTitleArray);
             console.log('Done Title Data');
             //Resolving and Rejecting if statements, to determine wait for data
         //});//forEach
       });//dbs end
       dbs.close();
-	   });//Promise End
-	return myPromise;
+     });//Promise End
+  return myPromise;
 
 }//getExtendedData End
 
@@ -377,7 +393,7 @@ function getTitlePrincipalData(inputID) {
 
 
 
-//New function 4-22-18
+//Updated 4/25/18
 function getPersonData(inputID) {
 
     var rowlength; //to hold rowlength
@@ -385,34 +401,36 @@ function getPersonData(inputID) {
     var sqlItem = 'SELECT primary_name, birth_year, death_year, primary_profession, known_for_titles FROM Names WHERE nconst='+'"'+inputID+'"';
     sqlPeopleArray = []; //resets the sqlHolder array to be empty so data doesn't repeat
     console.log(sqlItem);
-    console.log(sqlPeopleArray+'Before dbs')
-	var myPromise = new Promise(function(resolve, reject) {
-		var dbs = new sqlite3.Database('./imdb.sqlite3', (err) => {
-			if(err) {
-				return console.error(err.message);
-			}//if(err) end
-		});//dbs end
+    console.log(sqlPeopleArray+'Before dbs');
+
+  var myPromise = new Promise(function(resolve, reject) {
+    var dbs = new sqlite3.Database('./imdb.sqlite3', (err) => {
+      if(err) {
+        return console.error(err.message);
+      }//if(err) end
+    });//dbs end
 
         dbs.all(sqlItem, [], (err, rows) => {
-        	console.log(rows);
-        	console.log('rowlength = rows.length')
-        	var rowlength = rows.length;
-        	if(err) {// logs error
+          console.log(rows);
+          console.log('rowlength = rows.length')
+          var rowlength = rows.length;
+          if(err) {// logs error
             console.log(err);
-        	}//if(err)
+          }//if(err)
 
-			//reject promise if no rows to prevent infinite loop
-			if(rowlength < 1) {reject('Unsuitable Data for usage because of 0 length!');}
+      //reject promise if no rows to prevent infinite loop
+      if(rowlength < 1) {reject('Unsuitable Data for usage because of 0 length!');}
 
-			//fill sqlHolder with data from each row
-			rows.forEach((row) => { currentrowlength += 1;
-				sqlPeopleArray.push({
-					primary_name: row.primary_name,
-					primary_profession: row.primary_profession,
-					birth_year: row.birth_year,
-					death_year: row.death_year,
-					known_for_titles: row.known_for_titles
-				});//sql push
+      //fill sqlHolder with data from each row
+      rows.forEach((row) => { currentrowlength += 1;
+        sqlPeopleArray.push({
+          primary_name: row.primary_name,
+          primary_profession: row.primary_profession,
+          birth_year: row.birth_year,
+          death_year: row.death_year,
+          known_for_titles: row.known_for_titles,
+          known_for_titles2: getTitleNames(row.known_for_titles)
+        });//sql push
 
             if(currentrowlength == rowlength) { resolve(sqlPeopleArray); console.log('Done Person Data');}
             //Resolving and Rejecting if statements, to determine wait for data
@@ -420,10 +438,190 @@ function getPersonData(inputID) {
     });//dbs end
     dbs.close();
     console.log(sqlPeopleArray+'after dbs')
-	});//Promise End
-	return myPromise;
+  });//Promise End
+  return myPromise;
 }//getPersonData End
 
+//UPDATED CODE BY NOU
+function posterDataFunction(title_id) {
+    var myPromise = new Promise(function(resolve, reject) {
+        var req_url = {
+        host: 'www.imdb.com',
+        path: '/title/' + title_id + '/'
+    };
+    var req = https.request(req_url, (res) => {
+        var body = '';
+        res.on('data', (chunk) => {
+            body += chunk;
+        });
+        res.on('end', () => {
+            
+            var poster_link_pos = body.indexOf('<a href="/title/' + title_id + '/mediaviewer/');
+            var poster_img_pos = body.indexOf('<img', poster_link_pos);
+            var poster_src_pos = body.indexOf('src=', poster_img_pos) + 5;
+            var poster_end_pos = body.indexOf('"', poster_src_pos);
+        
+            var poster_url = url.parse(body.substring(poster_src_pos, poster_end_pos));
+            if (poster_url.host !== null) {
+                posterImage = {host: poster_url.host, path: poster_url.pathname}; 
+                console.log(posterImage+' adasdasdasda');
+                resolve(posterImage);
+            }
+            else {
+                posterImage = '/default_poster.jpg'; 
+                console.log('poster not found');
+                resolve(posterImage);
+            }
+        });
+    });
+
+    req.on('error', (err) => {
+        console.log(err);
+    });
+
+    req.on('timeout', () => {
+        console.log('timeout', null);
+        req.abort();
+    });
+
+    req.setTimeout(5000);
+    req.end();
+    });//Promise End
+    return myPromise;
+}//End
+
+function posterPersonDataFunction(name_id) {
+    var myPromise = new Promise(function(resolve, reject) {
+        var req_url = {
+        host: 'www.imdb.com',
+        path: '/name/' + name_id + '/'
+    };
+    var req = https.request(req_url, (res) => {
+        var body = '';
+        res.on('data', (chunk) => {
+            body += chunk;
+        });
+        res.on('end', () => {
+            var poster_link_pos = body.indexOf('<a href="/name/' + name_id + '/mediaviewer/');
+            var poster_img_pos = body.indexOf('<img', poster_link_pos);
+            var poster_src_pos = body.indexOf('src=', poster_img_pos) + 5;
+            var poster_end_pos = body.indexOf('"', poster_src_pos);
+            var poster_url = url.parse(body.substring(poster_src_pos, poster_end_pos));
+            if (poster_url.host !== null) {
+                posterImage = {host: poster_url.host, path: poster_url.pathname}; 
+                console.log(posterImage+' personDataFunction');
+                resolve(posterImage);}
+            else {
+                posterImage = '/default_poster.jpg'; 
+                console.log('poster not found');
+                resolve(posterImage);
+            }
+        });
+    });
+
+    req.on('error', (err) => {
+        console.log(err);
+    });
+
+    req.on('timeout', () => {
+        console.log('timeout', null);
+        req.abort();
+    });
+
+    req.setTimeout(5000);
+    req.end();
+    });//Promise End
+    return myPromise;
+}//End
+function getTitleNames(names) {
+    var rowlength; //to hold rowlength
+    var currentrowlength = 0;
+    var nameSplit = names.split(',');
+    var nameBlock = [];
+    var sqlItem;
+
+    nameSplit.forEach((nameItem) => {
+    sqlItem = 'SELECT tconst, primary_title FROM Titles WHERE tconst = "'+nameItem+'"';
+    
+        var dbs = new sqlite3.Database('./imdb.sqlite3', (err) => {
+            if(err) {
+                return console.error(err.message);
+            }//if(err) end
+        });//dbs end
+
+        dbs.all(sqlItem, [], (err, rows) => {
+            var rowlength = rows.length;
+            if(err) {// logs error
+                console.log(err);
+            }//if(err)
+
+            //reject promise if no rows to prevent infinite loop
+            if(rowlength < 1) { currentrowlength = 1; rowlength = 1;
+
+            }
+            else {
+                //fill sqlHolder with data from each row
+                rows.forEach((row) => { currentrowlength += 1;
+                    nameBlock.push({
+                        tconst: row.tconst,
+                        primary_title: row.primary_title
+                    });//sql push
+                });//forEach
+            }//else
+
+            //if(currentrowlength == rowlength) { resolve(sqlPeopleArray); console.log('Done Person Data');}
+
+        });//dbs.all
+    dbs.close();
+});
+    return nameBlock;
+}
+
+function getCastNames(names) {
+    var rowlength; //to hold rowlength
+    var currentrowlength = 0;
+    var nameSplit = names.split(',');
+    var nameBlock = [];
+    var sqlItem;
+
+    nameSplit.forEach((nameItem) => {
+    sqlItem = 'SELECT nconst, primary_name FROM Names WHERE nconst = "'+nameItem+'"';
+    
+        var dbs = new sqlite3.Database('./imdb.sqlite3', (err) => {
+            if(err) {
+                return console.error(err.message);
+            }//if(err) end
+        });//dbs end
+
+        dbs.all(sqlItem, [], (err, rows) => {
+            var rowlength = rows.length;
+            if(err) {// logs error
+                console.log(err);
+            }//if(err)
+
+            //reject promise if no rows to prevent infinite loop
+            if(rowlength < 1) { currentrowlength = 1; rowlength = 1;
+                
+            }
+            else {
+                //fill sqlHolder with data from each row
+                rows.forEach((row) => { currentrowlength += 1;
+                    nameBlock.push({
+                        nconst: row.nconst,
+                        primary_name: row.primary_name
+                    });//sql push
+                    console.log(row.nconst+' '+row.primary_name);
+                });//forEach
+            }//else
+
+            //if(currentrowlength == rowlength) { resolve(sqlPeopleArray); console.log('Done Person Data');}
+
+        });//dbs.all
+    dbs.close();
+});
+    return nameBlock;
+}
+//END UPDATED CODE BY NOU
 
 function updateData(sql) { //gets the SQL data
   var dbs = new sqlite3.Database('./imdb.sqlite3', (err) => {
